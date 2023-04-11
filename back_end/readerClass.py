b1fc9339fa9dd854011b16eb7b9565ef1bd5b101
BHOTW-DESK <mdsheik633@hotmail.com>
2023-04-10 23:08:23 -0400

    add read and write function

---
+++ back_end/readerClass.py
@@ -0,0 +1,21 @@
+import sysConfig
+
+class readerClass:
+    def __init__(self):
+
+    def destroy():
+        GPIO.cleanup()
+
+    def read(self):
+        print("TAP to read Data!")
+        id, text = reader.read()
+        return id, text
+
+
+    def write(self):
+        data = input('Programing new Data /n Full Name:')
+        print("TAP to write new Data!")
+        reader.write(data)
+        print("Data writing is complete.")
+
+    def attendance(self):
