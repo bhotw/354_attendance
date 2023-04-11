a101ed6e35f0928c0d02a82a5ffd3e661b4fd5ca
BHOTW-DESK <mdsheik633@hotmail.com>
2023-04-10 22:52:48 -0400

    started adding the back end system basically how the reader write and read on the tag.

---
+++ back_end/functions.py
@@ -0,0 +1,16 @@
+
+import sysConfig
+
+def get_time():
+    time.time()
+
+    year = str(time.strftime('%Y', time.localtime(time.time)))
+    month = str(time.strftime('%m', time.localtime(time.time)))
+    day = str(time.strftime('d', time.localtime(time.time)))
+    hour = str(time.strftime('%H',time.localtime(time.time())))
+	minute = str(time.strftime('%M',time.localtime(time.time())))
+	second = str(time.strftime('%S',time.localtime(time.time())))
+
+    current_time = hour + '.' + minute + '.' + second + " : " + month + '.' + day + '.' + year
+
+    return current_time
---
+++ back_end/sysConfig.py
@@ -0,0 +1,12 @@
+# System Configuration
+
+ import time
+ from tts import TTS
+
+ import RPi.GPIO as GPIO
+ from mfrc522 import SimpleMFRC522
+ from luma.core.interface.serial import spi, noop
+
+ serial = spi(port = 0, device = 1, gpio = nppp())
+
+ reader = SimpleMFRC522()
