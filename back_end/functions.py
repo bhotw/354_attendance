6775d491011079952ef9341fc30f3bc157b57b05
BHOTW-DESK <mdsheik633@hotmail.com>
2023-04-13 00:44:28 -0400

    some inconsistent use of tab error

--- back_end/functions.py
+++ back_end/functions.py
@@ -8,7 +8,7 @@ def get_time():
     month = str(time.strftime('%m', time.localtime(time.time())))
     day = str(time.strftime('d', time.localtime(time.time())))
     hour = str(time.strftime('%H',time.localtime(time.time())))
-	minute = str(time.strftime('%M',time.localtime(time.time())))
+	#minute = str(time.strftime('%M',time.localtime(time.time())))
 	second = str(time.strftime('%S',time.localtime(time.time())))
 
     current_time = hour + '.' + minute + '.' + second + " : " + month + '.' + day + '.' + year
