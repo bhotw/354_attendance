ef3455eb5fc52e90d5ae3700635435656f361fe0
Md Sheik <mdsheik633@hotmail.com>
2023-04-14 20:54:39 -0400

    brought everything into reader class and see how that works

--- back_end/functions.py
+++ back_end/functions.py
@@ -1,18 +1,18 @@
-
-import sysConfig as sys
-# import time
-
-def get_time():
-    # sys.time.time()
-
-    year = str(sys.time.strftime('%Y', sys.time.localtime(sys.time.time())))
-    month = str(sys.time.strftime('%m', sys.time.localtime(sys.time.time())))
-    day = str(sys.time.strftime('d', sys.time.localtime(sys.time.time())))
-    hour = str(sys.time.strftime('%H', sys.time.localtime(sys.time.time())))
-    minute = str(sys.time.strftime('%M', sys.time.localtime(sys.time.time())))
-    second = str(sys.time.strftime('%S', sys.time.localtime(sys.time.time())))
-
-    current_time = hour + '.' + minute + '.' + second + " : " + month + '.' + day + '.' + year
-    # current_time = se.time.localtime(se.time.time)
-
-    return current_time
+#
+# import sysConfig as sys
+# # import time
+#
+# def get_time():
+#     # sys.time.time()
+#
+#     year = str(sys.time.strftime('%Y', sys.time.localtime(sys.time.time())))
+#     month = str(sys.time.strftime('%m', sys.time.localtime(sys.time.time())))
+#     day = str(sys.time.strftime('d', sys.time.localtime(sys.time.time())))
+#     hour = str(sys.time.strftime('%H', sys.time.localtime(sys.time.time())))
+#     minute = str(sys.time.strftime('%M', sys.time.localtime(sys.time.time())))
+#     second = str(sys.time.strftime('%S', sys.time.localtime(sys.time.time())))
+#
+#     current_time = hour + '.' + minute + '.' + second + " : " + month + '.' + day + '.' + year
+#     # current_time = se.time.localtime(se.time.time)
+#
+#     return current_time
--- back_end/readerClass.py
+++ back_end/readerClass.py
@@ -1,22 +1,31 @@
-import functions
-function = functions.get_time()
+import time
+from tts import TTS
+
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+from luma.core.interface.serial import spi, noop
+
+serial = spi(port = 0, device = 1, gpio = nppp())
+
+reader = SimpleMFRC522()
+
 
 class readerClass:
     # def __init__(self):
     #     self
     def destroy(self):
-        function.GPIO.cleanup()
+        GPIO.cleanup()
 
     def read(self):
         print("TAP to read Data!")
-        id, text = function.reader.read()
+        id, text = reader.read()
         return id, text
 
 
     def write(self):
         data = input('Programing new Data /n Full Name:')
         print("TAP to write new Data!")
-        function.reader.write(data)
+        reader.write(data)
         print("Data writing is complete.")
 
     def get_action(self, s):
@@ -26,6 +35,22 @@ class readerClass:
         elif (s == "out"):
             action = "Sign Out"
         return action
+
+    def get_time(self):
+        # sys.time.time()
+
+        year = str(time.strftime('%Y', time.localtime(time.time())))
+        month = str(time.strftime('%m', time.localtime(time.time())))
+        day = str(time.strftime('d', time.localtime(time.time())))
+        hour = str(time.strftime('%H', time.localtime(time.time())))
+        minute = str(time.strftime('%M', time.localtime(time.time())))
+        second = str(time.strftime('%S', time.localtime(time.time())))
+
+        current_time = hour + '.' + minute + '.' + second + " : " + month + '.' + day + '.' + year
+        # current_time = se.time.localtime(se.time.time)
+
+        return current_time
+
 
     def attendance(self, action):
         attendance_statistics = {}
