a2aac7936bc48ad502f60c50d0d7092cb7b71ffd
Md Sheik <mdsheik633@hotmail.com>
2023-04-14 22:26:18 -0400

    test

--- back_end/readerClass.py
+++ back_end/readerClass.py
@@ -3,9 +3,9 @@ import time
 
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-from luma.core.interface.serial import spi, noop
+# from luma.core.interface.serial import spi, noop
 
-serial = spi(port = 0, device = 1, gpio = noop())
+# serial = spi(port = 0, device = 1, gpio = noop())
 
 reader = SimpleMFRC522()
 
