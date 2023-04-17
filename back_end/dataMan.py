9a6d1e92d29d70fb6836e032cf0a55fa4e668122
Md Sheik <mdsheik633@hotmail.com>
2023-04-17 14:44:38 -0400

    cursor issues

--- back_end/dataMan.py
+++ back_end/dataMan.py
@@ -1,5 +1,5 @@
 
-from back_end.dataBaseConfig import cursor as newDB
+from back_end.dataBaseConfig import cursor
 # import dataBaseConfig as data_base
 
 # newDB = data_base.cursor()
@@ -8,15 +8,15 @@ class DataMan:
     # def __init__(self):
 
     def createTable(self, table_name):
-        newDB.execute("CREATE TABLE (%S) (ID integer, firstName text, lastName text, Action text, Role text", table_name)
+        cursor.execute("CREATE TABLE (%S) (ID integer, firstName text, lastName text, Action text, Role text", table_name)
 
     def addToTable(self, meta_data, table_name):
         # reader_id , first_name, last_name, action, role = meta_data
         # newDB.execute("INSERT INTO (%s) VALUES('reader_id', 'first_name','last_name', 'action', 'role' )", (reader_id , first_name, last_name, action, role) )
         #
-        newDB.execute("INSERT INTO Students VALUES('meta_data')")
+        cursor.execute("INSERT INTO Students VALUES('meta_data')")
 
 
     def print(self):
-        print(newDB.fetchone())
+        print(cursor.fetchone())
 
