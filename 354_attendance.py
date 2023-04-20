484b765256cb4f8e428913675521a12351ec98fd
BHOTW-DESK <mdsheik633@hotmail.com>
2023-04-20 02:10:50 -0400

    fixed what i broke /

--- 354_attendance.py
+++ 354_attendance.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-
 @app.route("/home")
 def home():
+    return render_template('home.html')
+
+
+@app.route("/")
+def hello():
     return render_template('home.html')
 
 @app.route("/sign_in")
