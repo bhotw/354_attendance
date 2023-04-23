eb1a967f7cd35202c7d28b94064c5832952dce43
Md Sheik <mdsheik633@hotmail.com>
2023-04-23 02:46:24 -0400

    presting info

--- 354_attendance.py
+++ 354_attendance.py
@@ -25,34 +25,25 @@ def sign_out():
     return render_template('sign_out.html')
 
 
-class Get_info():
-
-    @expose("/get_info")
-    def get_info(self):
-        if request.method == 'GET':
-            # return render_template('get_info.html')
-            def present_info():
-                yield render_template('get_info.html')
-                reader_id, reader_name = ReaderClass.read("self")
-                data = [reader_id, reader_name]
-                session['get_info']=data
-                yield render_template('get_info.html', reader_id=reader_id, reader_name=reader_name)
-            return Response(stream_with_context(present_info()))
-
-
-    @expose("present_info")
-    def present_info(self):
-        if request.method == 'GET':
-            data = session['get_info']
-            reader_id = data[0]
-            reader_name = data[1]
-            return render_template('present_info.html', reader_id=reader_id, reader_name=reader_name )
-
-
 @app.route("/get_info", methods=['GET', 'POST'])
+def get_infor():
+    if request.method == 'GET':
+        # return render_template('get_info.html')
+        def present_info():
+            yield render_template('get_info.html')
+            reader_id, reader_name = ReaderClass.read("self")
+            data = [reader_id, reader_name]
+            flask.session['get_info']=data
+            yield render_template('get_info.html', reader_id=reader_id, reader_name=reader_name)
+        return Response(stream_with_context(present_info()))
 
 @app.route("/present_info")
-
+def present_info():
+    if request.method == 'GET':
+        data = flask.session['get_info']
+        reader_id = data[0]
+        reader_name = data[1]
+        return render_template('present_info.html', reader_id=reader_id, reader_name=reader_name )
 
 @app.route("/register")
 def register():
