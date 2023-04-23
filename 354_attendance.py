4dc6feb27ff49e076b3fbf15c678089b8e4118b6
Md Sheik <mdsheik633@hotmail.com>
2023-04-23 16:50:36 -0400

    still trying to fix presnting info

--- 354_attendance.py
+++ 354_attendance.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
 from flask import Response, stream_with_context, redirect, session
+from turbo_flask import Turbo
 from back_end.readerClass import ReaderClass
 
 
 
 app = Flask(__name__)
+turbo = Turbo(app)
 app.secret_key = "hello"
 
 
@@ -30,14 +32,23 @@ def sign_out():
 @app.route("/get_info", methods=['GET', 'POST'])
 def get_infor():
     if request.method == 'GET':
-        # return render_template('get_info.html')
-        def present_info():
-            yield render_template('get_info.html')
+
+
+        if turbo.can_stream():
             reader_id, reader_name = ReaderClass.read("self")
-            data = [reader_id, reader_name]
-            session["getinfo"]=data
-            yield render_template('get_info.html', reader_id=reader_id, reader_name=reader_name)
-        return Response(stream_with_context(present_info()))
+            return turbo.stream(
+                turbo.append(render_template('get_info.html', reader_id = reader_id, reader_name = reader_name), target='todos'),
+            )
+        else:
+            return render_template('get_info.html')
+        # # return render_template('get_info.html')
+        # def present_info():
+        #     yield render_template('get_info.html')
+        #     reader_id, reader_name = ReaderClass.read("self")
+        #     data = [reader_id, reader_name]
+        #     session["getinfo"]=data
+        #     yield render_template('get_info.html', reader_id=reader_id, reader_name=reader_name)
+        # return Response(stream_with_context(present_info()))
 
 @app.route("/present_info", methods=['GET', 'POST'])
 def present_info():
