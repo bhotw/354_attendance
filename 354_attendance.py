150bbb07a5d32ef99432efda2db1405d1cba23c5
Md Sheik <mdsheik633@hotmail.com>
2023-04-23 16:51:39 -0400

    still trying to fix presnting info

--- 354_attendance.py
+++ 354_attendance.py
@@ -33,9 +33,9 @@ def sign_out():
 def get_infor():
     if request.method == 'GET':
 
-
+        reader_id, reader_name = ReaderClass.read("self")
         if turbo.can_stream():
-            reader_id, reader_name = ReaderClass.read("self")
+
             return turbo.stream(
                 turbo.append(render_template('get_info.html', reader_id = reader_id, reader_name = reader_name), target='todos'),
             )
