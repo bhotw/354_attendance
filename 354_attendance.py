59e1d3b62a8221101da207c47f235a17fa8bdba0
Md Sheik <mdsheik633@hotmail.com>
2023-04-23 17:22:18 -0400

    test

--- 354_attendance.py
+++ 354_attendance.py
@@ -52,7 +52,7 @@ def get_infor():
             reader_id, reader_name = ReaderClass.read("self")
             data = [reader_id, reader_name]
             session["getinfo"]=data
-            yield render_template('present_info.html')
+            yield render_template('get_info.html', reader_id=reader_id, reader_name=reader_name)
         return Response(stream_with_context(present_info()))
 
 @app.route("/present_info", methods=['GET', 'POST'])
