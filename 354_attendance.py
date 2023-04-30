d5adcddce4d488b741666785ff2423fda14b4d94
Md Sheik <mdsheik633@hotmail.com>
2023-04-30 15:47:11 -0400

    issues with stream with context

--- 354_attendance.py
+++ 354_attendance.py
@@ -23,26 +23,26 @@ def hello():
 @app.route("/sign_in", methods=['GET', 'POST'])
 def sign_in():
     if request.method == 'GET':
-        def present_info():
+        def present_sign_in():
             yield render_template('sign_in.html')
             reader_id, reader_name = ReaderClass.read("self")
             # message = Command.sign_in("self",reader_id,reader_name )
             message = "Id: "+reader_id + "name:" + reader_name+"You have singed out at:" + ReaderClass.get_time("self")  # just for testing
             yield render_template('present_message.html', action="sign_in", message=message)
-        return render_template(present_info())
+        return render_template(stream_with_context(present_sign_in()))
 
 @app.route("/sign_out", methods=['GET', 'POST'])
 def sign_out():
 
     if request.method == 'GET':
-        def present_info():
+        def present_sign_out():
             yield render_template('sign_out.html')
             reader_id, reader_name = ReaderClass.read("self")
             # message = Command.sign_out(reader_id, reader_name)
             message = "Id: "+reader_id + "name:" + reader_name+"You have singed out at:" + ReaderClass.get_time("self")  # just for testing
             yield render_template('present_message.html', action="sign_out", message=message)
 
-        return render_template('sign_out.html')
+        return render_template(stream_with_context(present_sign_out()))
 
 
 @app.route("/get_info", methods=['GET', 'POST'])
