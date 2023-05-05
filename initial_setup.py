1074e03c45ad170bc6144ce67160c95ba27d102c
Md Sheik <mdsheik633@hotmail.com>
2023-05-05 00:04:35 -0400

    started working on packaging the whole thing in a way so that we will have no issues when we are setig this up on the actual thing. I will bring the thing on next.

---
+++ initial_setup.py
@@ -0,0 +1,16 @@
+
+# When we are setting up the app if you run this file it will create all the tables that are needed.
+# or we name this something else and store it somewhere else so that we can have only one setup.py
+# that will make sure that we get all the denpendency downloaded and installed. and then we have to
+# fireup psql and create a database and run one more file to create all the tables and then are all set.
+# I don't think creating the database and the user on the database can be automated so, we will have to see.
+
+from back_end.dataBaseConfig import conn, cursor
+
+cursor.execute("CREATE TABLE mentors ("
+               "reader_id INT PRIMARY KEY NOT NULL, "
+               "reader_name VARCHAR ( 70 ) NOT NULL,"
+               ""
+               
+               ")")
+
