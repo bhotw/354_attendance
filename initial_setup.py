ec3084227de5b770c785237b56701ca0b0ae5895
bhotw <mdsheik633@hotmail.com>
2023-05-09 14:16:23 -0400

    add new stuff to initfoler

--- initial_setup.py
+++ initial_setup.py
@@ -7,10 +7,22 @@
 
 from back_end.dataBaseConfig import conn, cursor
 
-cursor.execute("CREATE TABLE mentors ("
+cursor.execute("CREATE TABLE [IF NOT EXISTS] mentors ("
                "reader_id INT PRIMARY KEY NOT NULL, "
                "reader_name VARCHAR ( 70 ) NOT NULL,"
+               "role VARCHAR ( 20 ) NOT NULL,"
+               "Date date NOT NULL,"
+               "Time time NOT NULL,"
                ""
                
+               ")")
+print("Mentos table creation successful.")
+
+cursor.execute("CREATE TABLE students ("
+               "reader_id INT PRIMARY KEY NOT NULL, "
+               "reader_name VARCHAR ( 70 ) NOT NULL,"
+               "role VARCHAR ( 20 ) NOT NULL,"
+               ""
+
                ")")
 
