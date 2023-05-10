b103b89b1c1a42184a9d1c7f55103de2d49fbd54
bhotw <mdsheik633@hotmail.com>
2023-05-09 20:08:31 -0400

    more stuff

--- initial_setup.py
+++ initial_setup.py
@@ -5,7 +5,23 @@
 # fireup psql and create a database and run one more file to create all the tables and then are all set.
 # I don't think creating the database and the user on the database can be automated so, we will have to see.
 
-from back_end.dataBaseConfig import conn, cursor
+
+import psycopg2
+
+data_base = str(input("Database name: "))
+database_host = str(input("host = "))
+database_port = str(input("port"))
+database_user = str(input("username: "))
+database_password = str(input("password: "))
+
+conn = psycopg2.connect(database=data_base,
+                        host=database_host,
+                        user=database_user,
+                        password=database_password,
+                        port=database_port)
+
+cursor = conn.cursor()
+
 
 cursor.execute("CREATE TABLE [IF NOT EXISTS] mentors ("
                "reader_id INT PRIMARY KEY NOT NULL, "
