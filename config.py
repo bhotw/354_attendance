dafd38b2fdb4ee663eac67d7d4f765c4a0f616d6
Md Sheik <mdsheik633@hotmail.com>
2023-10-19 17:54:19 -0400

    made a lot of changes that will make everything...

--- config.py
+++ config.py
@@ -1,13 +1,6 @@
 import os
 
-DB_USERNAME = ''
-DB_PASSWORD = ''
-DB_HOST = 'localhost'
-DB_PORT = '5432'
-DB_NAME = '354_attendance'
-
-SQLALCHEMY_DATABASE_URL = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URI")
 SQLALCHEMY_TRACK_MODIFICATION = False
 
-SECRE_KEY = 'This_is_a_secret_key'
+SECRE_KEY = os.getenv('SECRE_KEY')
---
+++ routes.py
@@ -0,0 +1,135 @@
+from attendance import app, db
+from flask_sqlalchem import SQLAlchemy
+
+from flask import Flask, render_template, request
+from flask import Response, stream_with_context, redirect, url_for
+
+from back_end.readerClass import ReaderClass
+from back_end.command import Command
+import time
+
+
+
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/")
+def hello():
+    return redirect('/home')
+
+@app.route("/sign_in", methods=['GET', 'POST'])
+def sign_in():
+    if request.method == 'GET':
+        def present_sign_in():
+            yield render_template('sign_in.html')
+            reader_id, reader_name = ReaderClass.read("self")
+            present_date, present_time = ReaderClass.get_time("self")
+            message = [reader_name, "Sign in", present_time, present_date]
+            # message = Command.sign_in("self",reader_id,reader_name)
+            yield render_template('present_message.html', action="sign_in", message=message)
+            time.sleep(30)
+            yield render_template('home.html')
+            ReaderClass.destroy("self")
+        return Response(stream_with_context(present_sign_in()))
+
+
+@app.route("/sign_out", methods=['GET', 'POST'])
+def sign_out():
+
+    if request.method == 'GET':
+        def present_sign_out():
+            yield render_template('sign_out.html')
+            reader_id, reader_name = ReaderClass.read("self")
+
+            present_date, present_time = ReaderClass.get_time("self")
+            message = [reader_name, "Sign in", present_time, present_date]
+            # message = Command.sign_out("self",reader_id,reader_name)
+            yield render_template('present_message.html', action="sign_out", message=message)
+            time.sleep(30)
+            yield render_template('home.html')
+            ReaderClass.destroy("self")
+
+        return Response(stream_with_context(present_sign_out()))
+
+
+@app.route("/get_info", methods=['GET', 'POST'])
+def get_info():
+    if request.method == 'GET':
+
+        def present_info():
+            yield render_template('get_info.html')
+            reader_id, reader_name = ReaderClass.read("self")
+            message = Command.get_info("self",reader_id,reader_name)
+            yield render_template('present_message.html', action="info", message=message)
+            time.sleep(30)
+            yield render_template('home.html')
+            ReaderClass.destroy("self")
+        return Response(stream_with_context(present_info()))
+
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = Registration()
+    card_id = ReaderClass.read()
+    if form.validate_on_submit():
+        user = User(id=card_id, name=form.name, role=form.role, email=form.email, phone=form.phone, emergency_contact=form.emergency_contact, emergency_phone=form.emergency_phone, parent_email=form.parent_email)
+        
+        db.session.add(user)
+        db.session.commit()
+
+        flash('New Member has been added to the team!!!')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Registation', form=form)
+
+
+
+#  this is the old code we shall see if the new code works. Only then we 
+#   we can get rid of the old code. I know they are all on github and on
+#   other branch but I still want to keep them here for the time bring. 
+    # if request.method == 'POST':
+    #     name = request.form["fullname"]
+    #     role = request.form["role"]
+    #     # def present_register():
+    #     #     yield render_template('register.html')
+    #     #     name = request.form("fullname")
+    #     #     role = request.form("role")
+    #     #     message = name + " " + role
+    #     #     print(message)
+    #     #     yield render_template('present_message.html', message=message)
+
+    #     # return Response(stream_with_context(present_register()))
+    #     print (name, " ", role)
+
+    # return render_template('register.html')
+
+@app.route("/status")
+def status():
+    if request.method == 'GET':
+        def present_status():
+            yield render_template('status.html')
+            reader_id, reader_name = ReaderClass.read("self")
+            message = Command.get_status("self",reader_id,reader_name)
+            yield render_template('present_message.html', action="status", message=message)
+            time.sleep(30)
+            yield render_template('home.html')
+            ReaderClass.destroy("self")
+
+        return Response(stream_with_context(present_status()))
+    return render_template('status.html')
+
+
+@app.route("/admin")
+def admin():
+    return render_template('admin.html')
+
+
+@app.route("/clear", methods=['GET'])
+def clear():
+    if request.method == 'GET':
+        ReaderClass.destroy("self")
+        return redirect(url_for('home'))
+
+
