b542c28597aac20c6412a3ac25ef6f343a568b72
Md Sheik <mdsheik633@hotmail.com>
2023-10-30 19:31:15 -0400

    adding by blueprint

--- attendance.py
+++ attendance.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from your_app.routes import your_blueprint
+
 from config import SQLALCHEMY_DATABASE_URI, SECRE_KEY
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
@@ -10,6 +12,8 @@ from back_end.controllers.registration import Registration
 
 
 app = Flask(__name__)
+app.register_blueprint(your_blueprint)
+
 
 # app.config.from_object('config')
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI  # Set the configuration
