9b1de9841384a79a7b40aad43eb2b00ffbf2278f
Md Sheik <mdsheik633@hotmail.com>
2023-10-30 20:30:14 -0400

    idk

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI" )
 SQLALCHEMY_TRACK_MODIFICATION = False
 
-SECRE_KEY = os.getenv('SECRE_KEY')
+SECRE_KEY = "this is a secreate and i will keep that way"
 
 
 
