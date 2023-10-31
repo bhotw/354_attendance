88a12197e8a6f300c7f9845195cb57a3a1db7794
Md Sheik <mdsheik633@hotmail.com>
2023-10-30 21:22:36 -0400

    typo

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 
-SQLALCHEMY_DATABASE_URI = "postgresql://pitest:pi_test@localhost/attendance_test"
+SQLALCHEMY_DATABASE_URI = "postgresql://pitest:pi_test@localhost/attendance"
 SQLALCHEMY_TRACK_MODIFICATION = False
 
 SECRET_KEY = secrets.token_hex(16)
