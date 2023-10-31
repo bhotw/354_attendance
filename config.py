7ed9fc299fa5840ce4e32656d711b4829dfdce9d
Md Sheik <mdsheik633@hotmail.com>
2023-10-30 20:31:28 -0400

    addign stuff

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI" )
+SQLALCHEMY_DATABASE_URI = "postgresql://pitest:pi_test@localhost/attendance_test"
 SQLALCHEMY_TRACK_MODIFICATION = False
 
 SECRE_KEY = "this is a secreate and i will keep that way"
