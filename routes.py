7f4e87dc6660146a6ea875fcaccbc2df4dbf158d
Md Sheik <mdsheik633@hotmail.com>
2023-10-30 20:50:17 -0400

    idk

--- routes.py
+++ routes.py
@@ -9,7 +9,6 @@ from flask import Response, stream_with_context, redirect, url_for
 from back_end.readerClass import ReaderClass
 from back_end.command import Command
 from back_end.controllers.registration import Registration
-from back_end.models import User
 import time
 
 
