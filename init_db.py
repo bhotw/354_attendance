f6ac4fe6db2e3a8d072277857afd36cf94549f7e
Md Sheik <mdsheik633@hotmail.com>
2025-01-09 23:02:14 -0500

    fixing creating tables and showing that they were created

--- init_db.py
+++ init_db.py
@@ -1,10 +1,20 @@
-# this has to run before the app starts to run this file will create 
-# all the neccery databases
 from attendance import app
-from attendance import db
+from back_end.database import db
 from back_end.models import *
 from back_end.admin.models import *
+from sqlalchemy import inspect
 
 with app.app_context():
-    db.create_all()
+    db.create_all()  # This will create the tables
 
+    # Get the list of all tables in the database
+    inspector = inspect(db.engine)
+    tables = inspector.get_table_names()
+
+    # Print the tables to confirm their creation
+    print("Tables in the database:", tables)
+
+    # Optionally, you can check and print each table's columns
+    for table in tables:
+        columns = inspector.get_columns(table)
+        print(f"Columns in {table}: {[column['name'] for column in columns]}")
