76d15502c6a748487c1787aa24a5655c3bb14c2c
Md Sheik <mdsheik633@hotmail.com>
2025-02-03 17:29:46 -0500

    looks good tho i need to add two more function

--- admin/src/axiosInstance.js
+++ admin/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // Set your API base URL
+  baseURL: "http://192.168.1.27:5000", // Set your API base URL
   headers: {
     "Content-Type": "application/json",
   },
--- admin/src/components/Login.js
+++ admin/src/components/Login.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Login.css";
 import api from "../axiosInstance";
+import axios from "axios";
 
 const Login = () => {
   const [username, setUsername] = useState("");
