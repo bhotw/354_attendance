378c83785a741edaf3545a70d343f115b726fd5c
Md Sheik <mdsheik633@hotmail.com>
2025-02-03 17:49:56 -0500

    fixed the issue where i had to put in the ip my self now it gets it on it's own

--- admin/src/axiosInstance.js
+++ admin/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.1.27:5000", // Set your API base URL
+  baseURL: `http://${window.location.hostname}:5000`, // Set your API base URL
   headers: {
     "Content-Type": "application/json",
   },
