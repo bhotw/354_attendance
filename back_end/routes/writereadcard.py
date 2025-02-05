1c66e20aecabbd399f5d088bbe41457b09cb1c64
Md Sheik <mdsheik633@hotmail.com>
2025-02-04 22:12:39 -0500

    making it so that reader claens up right after it was used

--- back_end/routes/writereadcard.py
+++ back_end/routes/writereadcard.py
@@ -54,11 +54,11 @@ def read_card():
 def read_user():
     try:
         card_id = reader.read_id()
-        reader.destroy()
+
         if not card_id:
             return jsonify({"status": "error", "message": "Need a valid card id."}), 400
         user = User.query.filter_by(card_id=card_id)
-
+        reader.destroy()
         if not user:
             return jsonify({"status": "error", "message": "Card is not in our team."}), 400
 
