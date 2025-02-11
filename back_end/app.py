bf92d48b5a779989c2d9092648ed46a942ac9139
Md Sheik <mdsheik633@hotmail.com>
2025-02-10 19:01:31 -0500

    session stuff was not tested trying to fix that

--- back_end/app.py
+++ back_end/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from config import SQLALCHEMY_DATABASE_URI, SECRET_KEY
+from datetime import timedelta
 
 from extensions import db
 
