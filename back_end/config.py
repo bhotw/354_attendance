c43ea40a6c8ab9c133abdf476084799c35aeb47b
Md Sheik <mdsheik633@hotmail.com>
2025-02-17 18:32:42 -0500

    this one might have all the fixes

--- back_end/config.py
+++ back_end/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URI = os.getenv("db_url")
+SQLALCHEMY_DATABASE_URI = os.getenv("DB_URL")
 SQLALCHEMY_TRACK_MODIFICATION = False
 
 SECRET_KEY = secrets.token_hex(16)
