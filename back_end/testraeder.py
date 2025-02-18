4141c438dbfd61ce756e7fb49ce3cbeb8817fd6f
Md Sheik <mdsheik633@hotmail.com>
2025-02-17 22:13:48 -0500

    changes to testreader

--- back_end/testraeder.py
+++ back_end/testraeder.py
@@ -3,8 +3,8 @@ from readerClass import ReaderClass
 reader = ReaderClass()
 
 print("tap a card: ")
-card_id, card_name = reader.read()
+card_id, card_name = reader.read_id_name()
 print("card id: ", card_id, " card name: ", card_name)
 print("Tap a card for only card id: ")
-card_id = reader.read_id()
+card_id = reader.read_only_id()
 print("only card id: ", card_id)
