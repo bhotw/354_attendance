651819a4f44247042e6780b30a3d8131f9acc39f
Md Sheik <mdsheik633@hotmail.com>
2025-02-21 19:23:48 -0500

    trying out using eventlet for backend so that it is easy for the pi

--- admin/src/axiosInstance.js
+++ admin/src/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-//const API_BASE_URL = `http://${window.location.hostname}:5000`; // Set API base URL
+const API_BASE_URL = `http://${window.location.hostname}:5000`; // Set API base URL
 
-const API_BASE_URL = `http://192.168.1.27:5000`; // Set API base URL
+//const API_BASE_URL = `http://192.168.1.27:5000`; // Set API base URL
 
 
 const api = axios.create({
--- scripts/backend.sh
+++ scripts/backend.sh
@@ -4,4 +4,4 @@ cd "/home/attendance_user/354_attendance/back_end" || exit 1
 
 source "/home/attendance_user/354_attendance/backend_env/bin/activate"
 
-exec gunicorn -w 4 -b 0.0.0.0:5000 app:app
+exec gunicorn -k eventlet -w 1 -b 0.0.0.0:5000 app:app
